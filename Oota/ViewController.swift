5f8d0f30ea2ce8d8f3112b7105e3662936ce687a
= <tilaksharma1114@gmail.com>
2021-02-10 16:29:37 +0530

    Initial Commit

---
+++ Oota/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Oota
+//
+//  Created by Tilak Sharma on 10/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
